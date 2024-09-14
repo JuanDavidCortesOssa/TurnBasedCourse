1b61ee7f55e0727951e9c434f3c934efb9ceeb86
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2024-09-13 20:09:01 -0500

    asdf

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private float moveSpeed = 4f;
+
+    void Update()
+    {
+        float stoppingDistance = .1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4f, 0, 1f));
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
