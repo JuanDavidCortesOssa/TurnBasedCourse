adc5ca5a288a243d915cbb940dc6771ba459e3ba
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2024-09-22 17:50:03 -0500

    Unit selection visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
 
+    public event EventHandler OnSelectedUnitChanged;
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There are two action systems on the scene");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
 
     private void Update()
     {
@@ -24,12 +39,23 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (unitHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
 
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, new EventArgs());
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 
 }
